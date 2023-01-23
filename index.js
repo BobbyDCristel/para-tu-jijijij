fdfb2ea39ac802f9d281a5b56e4948d48658f744
BobbyDCristel <112992142+BobbyDCristel@users.noreply.github.com>
2023-01-22 20:34:18 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click',function () {
+    alert('Sabes que te quiero coger')
+});
+
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('click',() => {
+        alert('De que otra manera quieres que te de ;3');
+    });
+
+
